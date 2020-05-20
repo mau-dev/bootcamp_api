00d3dab37813eea3448cfe9de4c0009ba8b15740
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-05-21 00:34:32 +0800

    connected to mongoDB with Mongoose

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-//route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
+
+//Connect to database
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -26,4 +31,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // close server and exit process
+    server.close(() => process.exit(1));
+})

304a48f9df5c93975c63c750ccf63f308115a928
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-05-20 17:28:46 +0800

    test routes for bootcamps, controllers, logger middleware

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+//route files
+const bootcamps = require('./routes/bootcamps');
 
 //Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+
+// app.use(logger);
+
+//Dev logging middleware
+//run only in development environment
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
-);
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));

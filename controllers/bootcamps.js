ecb2196c59991b8decb31ac0fe4bf6625bc31ef7
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-05-28 13:58:55 +0800

    query string foradvanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,15 @@ const geocoder = require('../utils/geocoder');
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	
-		const bootcamps = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    const bootcamps = await query;
+		// const bootcamps = await Bootcamp.find();
 	
 		res
 		.status(200)

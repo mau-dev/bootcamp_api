8f85c78ee5ff7894d645986ce93283cc643f27cc
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-06-01 21:44:35 +0800

    courses in bootcamps with virtuals, bootcamps in courses with populate, cascade delete courses in relationship after removing a bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,6 +99,10 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now
     }
+
+  }, {
+      toJSON: { virtuals: true},
+      toObject: { virtuals: true}
   }
 );
 
@@ -126,5 +130,21 @@ BootcampSchema.pre('save', async function(next) {
     this.address = undefined;
     next();
   });
+
+   // Cascade delete courses ehen a bootcamp is deleted
+   BootcampSchema.pre('remove', async function (next) {
+       console.log(`Courses have been removed from bootcamp ${this._id}`);
+       await this.model('Course').deleteMany({ bootcamp: this._id });
+   });
+
+
+  // Reverse populate with virtuals
+BootcampSchema.virtual( 'courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
+
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);

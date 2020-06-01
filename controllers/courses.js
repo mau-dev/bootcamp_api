ec05dd56223c72130dc34fad86e31be0f1261dfe
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-06-01 23:23:04 +0800

    update and delete a course

--- controllers/courses.js
+++ controllers/courses.js
@@ -69,3 +69,43 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 		data: course
 	});
 });
+
+// @desc    Update new course
+// @route   PUT /api/v1/bootcamps/courses/:id
+// @access  Private
+
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+	let course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(new ErrorResponse(`No course found with the id of ${req.params.bootcampId}`, 404));
+	}
+
+	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: course
+	});
+});
+
+// @desc    Delete course
+// @route   DELETE /api/v1/course/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+	const course = await Course.findById(req.params.id);
+
+	if (!course) {
+		return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 404));
+	}
+
+	await course.remove();
+
+	res.status(200).json({
+		success: true,
+		data: {}
+	});
+});
